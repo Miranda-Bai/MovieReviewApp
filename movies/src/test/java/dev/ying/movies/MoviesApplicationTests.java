86311ddb6a7fa81e3d289dc1e3ae8e8cd3219e9d
Miranda <120993025+Miranda-Bai@users.noreply.github.com>
2023-06-11 15:21:01 +1200

    Initial commit

---
+++ movies/src/test/java/dev/ying/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ying.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
