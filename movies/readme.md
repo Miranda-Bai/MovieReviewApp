c0317250874700b75a25b1f7d1a94826f5b26614
Miranda <120993025+Miranda-Bai@users.noreply.github.com>
2023-06-12 15:05:53 +1200

    20230612

---
+++ movies/readme.md
@@ -0,0 +1,3 @@
+visit backend sever by http://localhost:8080/
+
+Formatting Java files: shift+option+f
--- movies/src/main/java/dev/ying/movies/MoviesApplication.java
+++ movies/src/main/java/dev/ying/movies/MoviesApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+// @EnableAutoConfiguration(exclude = { MongoAutoConfiguration.class }) // ignore mongodb auto configuration
+// @RestController
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
 	}
+	// an example about how to match request path
+	// @GetMapping("/")
+	// public String apiRoot() {
+	// return "Hello world!";
+	// }
 
 }
